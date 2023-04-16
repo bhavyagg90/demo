a4c0f376455c20976fd65bd544120378f2e1312d
bhavyagg90 <gbhavya18sliet@gmail.com>
2023-04-16 11:48:50 +0530

    again

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import './app.css'
+import React from "react";
+import "./App.css";
 // import Main from './Components/Main/Main'
 // import Footer from './Components/Footer/Footer'
-import Navbar from './Components/Navbar/Navbar'
+import Navbar from "./Components/Navbar/Navbar";
 // import Home from './Components/Home/Home'
 const App = () => {
   return (
     <>
-    <Navbar/>
-    {/* <Home/>
+      <Navbar />
+      {/* <Home/>
     <Main/>
     <Footer/> */}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
