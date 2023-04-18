a839f43d0160eeb8c0f45c449d0c8d4f4918f44c
bhavyagg90 <gbhavya18sliet@gmail.com>
2023-04-18 11:45:15 +0530

    home updated

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import "./App.css";
 // import Main from './Components/Main/Main'
 // import Footer from './Components/Footer/Footer'
 import Navbar from "./Components/Navbar/Navbar";
-// import Home from './Components/Home/Home'
+import Home from "./Components/Home/Home";
 const App = () => {
   return (
-    <>
+    <div>
       <Navbar />
-      {/* <Home/>
-    <Main/>
-    <Footer/> */}
-    </>
+      <Home />
+    </div>
   );
 };
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './navbar.css'
 import {MdOutlineTravelExplore} from 'react-icons/md'
 import {AiFillCloseCircle} from 'react-icons/ai'
 import {TbGridDots} from 'react-icons/tb'
 const Navbar = () => {
+  
+  const [active, setActive] = useState('navBar')
+
+  const showNav = ()=> {
+    setActive('navBar activeNavbar')
+  }
+
+  const removeNavbar = ()=> {
+    setActive('navBar')
+  }
+
   return (
     <section className='navBarSection'>
       <header className= "header flex">
@@ -14,7 +25,7 @@ const Navbar = () => {
 
         </div>
 
-        <div className='navBar'>
+        <div className={active}>
           <ul className='navLists flex'>
             <li className='navIten'>
               <a href="#" className="navLink">Home</a>
@@ -43,13 +54,13 @@ const Navbar = () => {
             </button>
           </ul>
 
-          <div className='closeNavbar'>
+          <div onClick={removeNavbar} className='closeNavbar'>
             <AiFillCloseCircle className='icon' /> 
 
           </div>
         </div>
 
-        <div className="toggleNavbar">
+        <div onClick={showNav} className="toggleNavbar">
           <TbGridDots className='icon'/>
         </div>
 
